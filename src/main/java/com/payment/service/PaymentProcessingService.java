0d40568ece3a95bf6a968a8c12fce347b3dc1a11
Srikanth Sara <srikanth.wankar@gmail.com>
2026-06-10 17:57:53 +0100

    remove sonar critical bug & created Constant class to define messages

--- src/main/java/com/payment/service/PaymentProcessingService.java
+++ src/main/java/com/payment/service/PaymentProcessingService.java
@@ -6,51 +6,40 @@ import com.common.event.PaymentSuccessEvent;
 import com.payment.entity.PaymentTransaction;
 import com.payment.kafka.PaymentEventProducer;
 import com.payment.repository.PaymentTransactionRepository;
+import com.payment.utils.Constants;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
 @Service
 @RequiredArgsConstructor
 public class PaymentProcessingService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaymentProcessingService.class);
     private final PaymentTransactionRepository repository;
-
     private final PaymentEventProducer producer;
 
-    public void processPayment(
-            OrderCreatedEvent event) {
+    public void processPayment(OrderCreatedEvent event) {
 
-        boolean paymentSuccess =
-                mockPaymentGateway(event);
+        if (Constants.PAYMENT_SUCCESS) {
 
-        if (paymentSuccess) {
-
-            String txnId =
-                    UUID.randomUUID().toString();
+            String txnId = UUID.randomUUID().toString();
 
             PaymentTransaction payment =
-
                     PaymentTransaction.builder()
-
                             .orderId(event.getOrderId())
-
                             .customerId(event.getCustomerId())
-
                             .amount(event.getTotalAmount())
-
                             .paymentType(event.getPaymentType())
-
                             .paymentProvider(event.getPaymentProvider())
-
-                            .paymentStatus("SUCCESS")
-
+                            .paymentStatus(Constants.PAYMENT_SUCCESS_STATUS)
                             .transactionId(txnId)
-
                             .createdAt(LocalDateTime.now())
-
                             .build();
 
             repository.save(payment);
@@ -58,65 +47,39 @@ public class PaymentProcessingService {
             producer.publishSuccess(
 
                     PaymentSuccessEvent.builder()
-
                             .orderId(event.getOrderId())
-
                             .customerId(event.getCustomerId())
-
                             .amount(event.getTotalAmount())
-
-                            .paymentStatus("SUCCESS")
-
+                            .paymentStatus(Constants.PAYMENT_SUCCESS_STATUS)
                             .transactionId(txnId)
-
                             .build());
 
-            System.out.println(
-                    "Payment processed successfully");
+            LOGGER.info("Payment processed successfully");
 
         } else {
 
             PaymentTransaction payment =
-
                     PaymentTransaction.builder()
-
                             .orderId(event.getOrderId())
-
                             .customerId(event.getCustomerId())
-
                             .amount(event.getTotalAmount())
-
-                            .paymentStatus("FAILED")
-
-                            .failureReason(
-                                    "Payment Gateway Failed")
-
+                            .paymentStatus(Constants.PAYMENT_FAILED_STATUS)
+                            .failureReason(Constants.PAYMENT_GATEWAY_FAILED_REASON)
                             .createdAt(LocalDateTime.now())
-
                             .build();
 
             repository.save(payment);
 
             producer.publishFailure(
-
                     PaymentFailedEvent.builder()
-
                             .orderId(event.getOrderId())
-
                             .customerId(event.getCustomerId())
-
                             .amount(event.getTotalAmount())
-
-                            .paymentStatus("FAILED")
-
-                            .reason("Payment Gateway Failed")
-
+                            .paymentStatus(Constants.PAYMENT_FAILED_STATUS)
+                            .reason(Constants.PAYMENT_GATEWAY_FAILED_REASON)
                             .build());
         }
     }
 
-    // MOCK GATEWAY
-    private boolean mockPaymentGateway(OrderCreatedEvent event) {
-        return true;
-    }
 }
+
---
+++ src/main/java/com/payment/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.payment.utils;
+
+public class Constants {
+
+    public static final Boolean PAYMENT_SUCCESS= true;
+    public static final String PAYMENT_SUCCESS_STATUS = "SUCCESS";
+    public static final String PAYMENT_FAILED_STATUS = "FAILED";
+    public static final String PAYMENT_GATEWAY_FAILED_REASON = "Payment Gateway Failed";
+}
+
